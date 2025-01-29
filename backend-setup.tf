7eb61b408780eaaddf41e29ca920cbe77f9715a8
Ngozi-N <jonathanengee@gmail.com>
2025-01-29 14:42:26 +0100

    Initial Commit of backend setup

---
+++ backend-setup.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "finance-tracker-tfstate"
+}
+
+resource "aws_s3_bucket_versioning" "state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "finance-tracker-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
