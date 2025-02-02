fcd0d1e21057506384feda3cbb53e4705fe43b8c
Ubuntu <ubuntu@ip-10-0-5-98.eu-west-2.compute.internal>
2025-02-02 21:52:34 +0000

    Commit of update

--- backend-setup.tf
+++ backend-setup.tf
@@ -1,30 +1,30 @@
-provider "aws" {
-  region = "eu-west-2"
-}
+#provider "aws" {
+#  region = "eu-west-2"
+#}
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "finance-tracker-tfstate"
-}
+#resource "aws_s3_bucket" "terraform_state" {
+#  bucket = "finance-tracker-tfstate"
+#}
 
-resource "aws_s3_bucket_versioning" "state_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#resource "aws_s3_bucket_versioning" "state_versioning" {
+#  bucket = aws_s3_bucket.terraform_state.id
+#  versioning_configuration {
+#    status = "Enabled"
+#  }
+#}
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "finance-tracker-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+#resource "aws_dynamodb_table" "terraform_locks" {
+#  name         = "finance-tracker-locks"
+#  billing_mode = "PAY_PER_REQUEST"
+#  hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+#  attribute {
+#    name = "LockID"
+#    type = "S"
+#  }
 
-  lifecycle {
-    ignore_changes = [read_capacity, write_capacity] # Prevent Terraform from failing if already exists
-  }
-}
+#  lifecycle {
+#    ignore_changes = [read_capacity, write_capacity] # Prevent Terraform from failing if already exists
+#  }
+#}
 
